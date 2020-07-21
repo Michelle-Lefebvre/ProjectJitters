49cd87e2cd1d07ba4582b4d7fe5cf3342e307980
MarkBoswick <Mark aaaaa asdf@bbbbbbbb.com>
2020-07-20 20:51:37 -0400

    additem and css

--- _admin.php
+++ _admin.php
@@ -2,6 +2,15 @@
 require_once '_setup.php';
 # this file is for /admin/* URL handlers
 
+// menu for administration functions
+$app->get('/adminmenu', function ($request, $response, $args) {
+    return $this->view->render($response, '/admin/adminmenu.html.twig');
+});
+
+
+
+
+
 //following is using addarticletest while following video about addperson and tristate form
 // STATE 1: first display  -- this is a get  -- just need to render the template
 $app->get('/additem', function ($request, $response, $args) {
@@ -12,12 +21,13 @@ $app->get('/additem', function ($request, $response, $args) {
 $app->post('/additem', function ($request, $response, $args) {
     
     // $app->post will display the form, following code will handle the form
-    $name = $request->getParam('name');
+    $itemName = $request->getParam('itemName');
     $description = $request->getParam('description');
     $categoryCode = $request->getParam('categoryCode');
     $inventoryFlag = $request->getParam('inventoryFlag');
     $quantityOnHand = $request->getParam('quantityOnHand');
     $price = $request->getParam('price');
+    $photofilepath = $request->getParam('photofilepath');
     
     //
     // FIXME: sanitize body - 1) only allow certain HTML tags, 2) make sure it is valid html
@@ -26,24 +36,38 @@ $app->post('/additem', function ($request, $response, $args) {
                
     $errorList = array();
 
-    if (strlen($name) < 10 || (strlen($name) >100) ) {
+    if (strlen($itemName) < 10 || (strlen($itemName) > 100) ) {
         $errorList[] = "Name must be between 10  and 100 characters long";
     }
     if (strlen($description) < 10  || strlen($description) > 250) {
         $errorList[] = "Description must be between 10 and 250 characters long";
     }
 
+    //print_r($categoryCode);
+    
 
+        // validate category code
+        $categoryList = DB::queryFirstRow("SELECT categoryCode FROM categorycodes
+         WHERE categoryCode = '$categoryCode'");  
+
+    if (!$categoryList) { 
+        $errorList[] = "Category Code does not exist";
+    } 
     
     if ($errorList) {
         return $this->view->render($response, '/admin/additem.html.twig',
                 ['errorList' => $errorList ]);
+
     } else {
         // response write is only allowed to be used with ajax
         //return $response->write("<p>Success</p>");
 
         // insert not working foreign key author id not found
-       // DB::insert( 'articles', ['id' => NULL, 'authorId' => 3, 'title' => $title, 'body' => $body ]);
+       DB::insert( 'items', ['itemid' => NULL, 'itemName' => $itemName,  'description' => $description,
+                   'categoryCode' => $categoryCode, 'inventoryFlag' => $inventoryFlag, 
+                   'quantityOnHand' => $quantityOnHand, 'price' => $price, 'photofilepath' => $photofilepath
+       
+       ]);
        // render the success template
        // no params to pass, but there could be... like an id or something that was
        // created
