5b3a0898cc11c1f9a963cbcf4866cddc28097b3f
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-07-24 12:31:42 -0400

    scrum meeting separate styles for index

--- _main.php
+++ _main.php
@@ -25,10 +25,10 @@ $app->get('/', function ($request, $response, $args) {
     // return $twig->render('login_success.html.twig');
 
     // return $twig->render('/admin/adminmenu.html.twig');
-    // return $twig->render('/admin/additem.html.twig');
+    return $twig->render('/admin/additem.html.twig');
     // return $twig->render('/admin/additem_success.html.twig');
     // return $twig->render('error_internal.html.twig');
-    return $twig->render('error_access_denied.html.twig');
+    // return $twig->render('error_access_denied.html.twig');
 });
 
 $app->get('/session', function ($request, $response, $args) {
