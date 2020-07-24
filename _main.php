7f2b48c5f728a97311813dcb19cf8a7017e635f8
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-07-24 17:12:01 -0400

    fix style conflicts

--- _main.php
+++ _main.php
@@ -18,19 +18,19 @@ $app->get('/', function ($request, $response, $args) {
     // return $response->write('this is main'); //for testing
     $loader = new FilesystemLoader(__DIR__ . '/templates');
     $twig = new Environment($loader);
-    return $twig->render('index.html.twig');
+    // return $twig->render('index.html.twig');
 
-    return $twig->render('register.html.twig');
-    return $twig->render('login.html.twig');
-    return $twig->render('login_success.html.twig');
+    // return $twig->render('register.html.twig');
+    // return $twig->render('login.html.twig');
+    // return $twig->render('login_success.html.twig');
 
-    return $twig->render('/admin/adminmenu.html.twig');
+    // return $twig->render('/admin/adminmenu.html.twig');
 
     return $twig->render('/admin/additem.html.twig');
     return $twig->render('/admin/additem_success.html.twig');
 
-    // return $twig->render('error_internal.html.twig');
-    // return $twig->render('error_access_denied.html.twig');
+    return $twig->render('error_internal.html.twig');
+    return $twig->render('error_access_denied.html.twig');
 });
 
 $app->get('/session', function ($request, $response, $args) {
