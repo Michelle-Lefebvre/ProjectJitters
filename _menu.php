98a417de1ac99b1b89e6b3c7b6db0ee2c2fbaacf
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-07-28 14:40:25 -0400

    connecting to db items table for menu

--- _menu.php
+++ _menu.php
@@ -1,3 +1,21 @@
 <?php
 require_once '_setup.php';
+
+
+// $sql = "SELECT itemId, itemName, price FROM items";
+// $result = DB::query($sql);
+$itemList = DB::queryFirstRow("SELECT * FROM items");  
+if ($itemList) {
+    echo "connected to db";
+    print_r($itemList);
+}
+// if ($itemList > 0) {
+//   // output data of each row
+//   while($row = fetch_assoc()) {
+//     echo "id: " . $row["itemId"]. " - Name: " . $row["itemName"]. " " . $row["price"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
 ?>
