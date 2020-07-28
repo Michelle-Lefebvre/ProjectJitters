5dc15db810f75d4b62f056390276a104f7535846
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-07-28 18:13:36 -0400

    working on menu, change text colour in header, modified link hover

--- _menu.php
+++ _menu.php
@@ -1,21 +1,44 @@
 <?php
 require_once '_setup.php';
 
-
-// $sql = "SELECT itemId, itemName, price FROM items";
-// $result = DB::query($sql);
-$itemList = DB::queryFirstRow("SELECT * FROM items");  
+$itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items");  
 if ($itemList) {
-    echo "connected to db";
-    print_r($itemList);
+    // echo "connected to db";
+    // print_r($itemList);
+    // print_r("<br><br><br><br>");
 }
-// if ($itemList > 0) {
-//   // output data of each row
-//   while($row = fetch_assoc()) {
-//     echo "id: " . $row["itemId"]. " - Name: " . $row["itemName"]. " " . $row["price"]. "<br>";
-//   }
-// } else {
-//   echo "0 results";
-// }
 
-?>
+$app->get('/menu', function ($request, $response, $args) {
+$categoryList = DB::query("SELECT categoryCode itemName, price, priceMed, priceLrg FROM items;");
+    if (!$categoryList) { 
+        $errorList[] = "Category Code does not exist";
+    } 
+    
+    if ($errorList) {
+        return $this->view->render($response, 'error_internal.html.twig',
+                ['errorList' => $errorList ]);
+
+    } else {
+
+    foreach ($categoryList as &$category) {
+        $itemName = $category['itemName'];
+        $price = $category['price'];
+        $priceMed = $category['priceMed'];
+        $priceLrg = $category['priceLrg'];
+        $categoryCode = $category['CAFE'];    
+    }
+   
+    return $this->view->render($response, 'menu.html.twig');
+    // return $this->view->render($response, 'menu.html.twig', ['CAFE' => $categoryCode, 'categoryList' => $categoryList]);
+    
+    while ($row = mysqli_fetch_assoc($categoryCode)) {
+        echo '<tr>';
+        echo '<td>'. $row['itemName'] .'</td>';
+        echo '<td>'. $row['price'] .'</td>';
+        echo '<td>'. $row['priceMed'] .'</td>';
+        echo '</tr>';
+    }
+
+    
+}
+});
