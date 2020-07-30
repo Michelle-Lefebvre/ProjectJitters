a664176116940034ce6dd2e8ebeb24fcc679f2f4
MarkBoswick <Mark aaaaa asdf@bbbbbbbb.com>
2020-07-30 09:52:38 -0400

    Update _main.php

--- _main.php
+++ _main.php
@@ -18,13 +18,13 @@ $app->get('/', function ($request, $response, $args) {
     // return $response->write('this is main'); //for testing
     $loader = new FilesystemLoader(__DIR__ . '/templates');
     $twig = new Environment($loader);
-    return $twig->render('index.html.twig');
+   // return $twig->render('index.html.twig');
 
     // return $twig->render('register.html.twig');
     // return $twig->render('login.html.twig');
     // return $twig->render('login_success.html.twig');
 
-    // return $twig->render('menu.html.twig');
+     return $twig->render('menu.html.twig');
 
     // return $twig->render('/admin/adminmenu.html.twig');
     return $twig->render('/admin/additem.html.twig');
