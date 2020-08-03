494281c1ad4c2c728680054cfe4507380fa455e0
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-08-03 16:45:43 -0400

    menu added all 4 tables and images

--- _menu.php
+++ _menu.php
@@ -2,9 +2,6 @@
 require_once '_setup.php';
 
 $app->get('/menu', function ($request, $response, $args) {
-    //$article = DB::queryFirstRow("SELECT a.id, a.authorId, a.creationTS, a.title, a.body, u.name "
-    //. "FROM articles as a, users as u WHERE a.authorId = u.id AND a.id = %d", $args['id']);
-    
     $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 'CAFE';");
   
     foreach ($itemList as &$item) {
@@ -13,11 +10,8 @@ $app->get('/menu', function ($request, $response, $args) {
 
     }
 
-    return $this->view->render($response, 'menu_mb.html.twig', ['list' => $itemList]);
+    return $this->view->render($response, 'menu.html.twig', ['list' => $itemList]);
 
     });
-
-    
-
 ?>
 
