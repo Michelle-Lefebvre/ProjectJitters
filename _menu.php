9d930829550964ec3e15a7d59e146a75e94c6423
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-08-04 10:49:22 -0400

    Fix index link in master and all error page

--- _menu.php
+++ _menu.php
@@ -1,16 +1,23 @@
 <?php
 require_once '_setup.php';
 
-$app->get('/menu', function ($request, $response, $args) {
-    $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 'CAFE';");
-  
-    foreach ($itemList as &$item) {
-        //formatting for each field done here
-        // no formatting yet.....
+// $app->get('/menu', function ($request, $response, $args) {
+//     $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 'CAFE';");
 
-    }
+//     return $this->view->render($response, 'menu.html.twig', ['list' => $itemList]);
 
-    return $this->view->render($response, 'menu.html.twig', ['list' => $itemList]);
-
-    });
+//     });
+        $app->get('/menu[/{catCode}]', function($request, $response, $args = '') {
+            $catCode= $args;
+            $itemListCafe = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = %s;", $catCode);
+        
+        $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 's%';",$catCode);
+      
+        
+        return $this->view->render($response, 'menu.html.twig', ['CAFE' =>$catCode, 'list' => $itemListCafe]);
+        return $this->view->render($response, 'menu.html.twig', ['TEAS' =>$catCode, 'teas' => $itemListCafe]);
+        return $this->view->render($response, 'menu.html.twig', ['BAKE' =>$catCode, 'food' => $itemListCafe]);
+        return $this->view->render($response, 'menu.html.twig', ['SAND' =>$catCode, 'sand' => $itemList]);
+    
+        });
 ?>
