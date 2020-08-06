6ca8d3831c9fb8896bd84d150573c18181a8ae00
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-08-06 10:32:39 -0400

    revise menu display in progress

--- _menu.php
+++ _menu.php
@@ -27,10 +27,14 @@ require_once '_setup.php';
                 FROM items WHERE categoryCode = 'CAFE';");
     $bakeList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg 
                 FROM items WHERE categoryCode = 'BAKE';");
+    $teaList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg 
+    FROM items WHERE categoryCode = 'TEAS';");
+    $sandList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg 
+    FROM items WHERE categoryCode = 'SAND';");
 
     //print_r($cafeList);
     //print_r($bakeList);
-     return $this->view->render($response, 'menu_mb.html.twig', ['cafeList' => $cafeList,'bakeList' => $bakeList ]);
+     return $this->view->render($response, 'menu.html.twig', ['cafeList' => $cafeList,'bakeList' => $bakeList,'teaList' => $teaList,'sandList' => $sandList ]);
 
      });
 
