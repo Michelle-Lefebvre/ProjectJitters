b0e0d62fba84c2b0860903e8e54c7050a7fdf784
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-08-06 20:12:56 -0400

    fix menu url

--- _menu.php
+++ _menu.php
@@ -1,27 +1,7 @@
 <?php
 require_once '_setup.php';
 
-// $app->get('/menu', function ($request, $response, $args) {
-//     $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 'CAFE';");
-
-//     return $this->view->render($response, 'menu.html.twig', ['list' => $itemList]);
-
-//     });
-        $app->get('/menu[/{catCode}]', function($request, $response, $args = '') {
-            $catCode= $args;
-            $itemListCafe = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = %s;", $catCode);
-        
-        $itemList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg FROM items WHERE categoryCode = 's%';",$catCode);
-      
-        
-        return $this->view->render($response, 'menu.html.twig', ['CAFE' =>$catCode, 'list' => $itemListCafe]);
-        return $this->view->render($response, 'menu.html.twig', ['TEAS' =>$catCode, 'teas' => $itemListCafe]);
-        return $this->view->render($response, 'menu.html.twig', ['BAKE' =>$catCode, 'food' => $itemListCafe]);
-        return $this->view->render($response, 'menu.html.twig', ['SAND' =>$catCode, 'sand' => $itemList]);
-    
-        });
-
- $app->get('/menu_mb', function ($request, $response, $args) {
+ $app->get('/menu', function ($request, $response, $args) {
      // 
      $cafeList = DB::query("SELECT categoryCode, itemName, price, priceMed, priceLrg 
                 FROM items WHERE categoryCode = 'CAFE';");
