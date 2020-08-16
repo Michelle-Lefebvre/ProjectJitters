7b01c259e99156b5826bbc4846f82e7d03b56e9a
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-08-16 16:33:14 -0400

    styles.css Fix position/style of error list messages for forms Index.php remove _article.php Change border of all error pages Delete unused files from .com (test & article)

--- _main.php
+++ _main.php
@@ -18,31 +18,57 @@ $app->get('/', function ($request, $response, $args) {
     // return $response->write('this is main'); //for testing
     $loader = new FilesystemLoader(__DIR__ . '/templates');
     $twig = new Environment($loader);
-    return $twig->render('index.html.twig');
-    
+    // return $twig->render('index.html.twig');
 
-    // return $twig->render('register.html.twig');
-    // return $twig->render('register_success.html.twig');
-    // return $twig->render('login.html.twig');
-    // return $twig->render('login_success.html.twig');
-    
-    //return $twig->render('profile.html.twig');
+    // Templates
+    // return $twig->render('error_access_denied.html.twig'); //ss
+    // return $twig->render('error_internal.html.twig'); //ss
+    // return $twig->render('error_not_found.html.twig');  //ss
 
-    // return $twig->render('menu.html.twig');
+    // return $twig->render('index.html.twig'); //ss
+
+    // return $twig->render('login_success.html.twig'); //ss
+    // return $twig->render('login.html.twig'); //ss
+    // return $twig->render('logout.html.twig'); //ss
+    // return $twig->render('master.html.twig');
+    // return $twig->render('menu.html.twig'); //ss
+    // return $twig->render('not_logged_in.html.twig'); //ss
+
+                // return $twig->render('profile_edit_success.html.twig');
+                // return $twig->render('profile.html.twig');
+
     // return $twig->render('cart.html.twig');
-    //return $twig->render('cartadditem.html.twig');
+    // return $twig->render('cartadditem.html.twig'); //ss
+    // return $twig->render('cartitems_delete_success.html.twig');
+    // return $twig->render('cartitems_delete.html.twig');
+    return $twig->render('cartitems_not_found.html.twig'); //ss
 
-    // return $twig->render('/admin/adminmenu.html.twig');
-    // return $twig->render('/admin/additem.html.twig');
-    // return $twig->render('/admin/additem_success.html.twig');
+    // return $twig->render('register_success.html.twig'); //ss
+    // return $twig->render('register.html.twig'); //ss
+    return $twig->render('rewards.html.twig');
 
-    // return $twig->render('/admin/adminmenu2.html.twig');
-    // return $twig->render('/admin/users_list.html.twig');
+    // Admin Templates
+    return $twig->render('adminmenu_notallowed.html.twig');
+    return $twig->render('adminmenu.html.twig');
 
-    // return $twig->render('error_internal.html.twig');
-    // return $twig->render('error_access_denied.html.twig');
+    return $twig->render('error_access_denied.html.twig');
     return $twig->render('error_not_found.html.twig');
-    
+
+    return $twig->render('items_addedit_success.html.twig');
+    return $twig->render('items_addedit.html.twig');
+    return $twig->render('items_delete_success.html.twig');
+    return $twig->render('items_delete.html.twig');
+    return $twig->render('items_list.html.twig');
+
+    return $twig->render('master.html.twig');
+    return $twig->render('not_found.html.twig');
+
+    return $twig->render('user_addedit.html.twig');
+    return $twig->render('user_delete.html.twig');
+    return $twig->render('users_addedit_success.html.twig');
+    return $twig->render('users_delete_success.html.twig');
+    return $twig->render('users_list.html.twig');
+    return $twig->render('users_master.html.twig');
 });
 
 $app->get('/session', function ($request, $response, $args) {
--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once '_setup.php'; // this must be first
 require_once '_main.php';
 require_once '_users.php';
 require_once '_admin.php';
-require_once '_articles.php';
 require_once '_menu.php';
 require_once '_cart.php';
 // Run app must be last time
